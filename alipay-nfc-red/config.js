706574937f170a43dcc74445e151496aa805abd0
Han <1655466387@qq.com>
2025-12-26 15:11:15 +0800

    test

---
+++ alipay-nfc-red/config.js
@@ -0,0 +1,9 @@
+window.ENV_CONFIG = {
+  // 支付宝用户ID
+  VITE_ALIPAY_USER_ID: "2088812641271501",
+  //碰一下红包分享活动的链接路径ID （ 示例：https://ur.alipay.com/_4btsDsC8UQ0z3FQzrPxjOr）
+  VITE_PATH_ID: "60z0501kmna7derjsd15"
+};
+
+// 作者：羚羊公子
+// 根据README.md中的教程然后填写自己的用户ID和链接路径ID
